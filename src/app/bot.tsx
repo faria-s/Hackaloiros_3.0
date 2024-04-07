35573ae7f1d422693d38348e3eadfccd1132dcbd
Afonso Martins <a106931@alunos.uminho.pt>
2024-04-07 01:39:08 +0100

    Feat: Site

--- src/app/bot.tsx
+++ src/app/bot.tsx
@@ -4,43 +4,57 @@ import React, { useState, useEffect } from 'react';
 import TickerData from './api'
 import fetch from 'node-fetch';
 import fetchTickerData from './api'
+import { TailSpin } from 'react-loader-spinner';
 
 function TradingBotVisualizer() {
     const [balance, setBalance] = useState(1000);   
+    const [btcBalance, setBtcBalance] = useState(0);
     const [btcPrice, setBtcPrice] = useState(0);
     const [action, setAction] = useState('manter');
+    const [loading, setLoading] = useState(true); // Estado para controlar a exibição do TailSpin
+
 
     useEffect(() => {
         const interval = setInterval(async () => {
             try {
                 const data = await fetchTickerData();
-                console.log(data); // Adicionando uma instrução de console para verificar os dados
-                setBtcPrice((data.bid + data.ask) / 2); // Usar a média entre bid e ask como preço
+                console.log(data);
+                setBtcPrice((data.bid + data.ask) / 2);
                 const newAction = await tradingBot(data.bid, data.ask);
                 setAction(newAction);
             } catch (error) {
                 console.error('Erro ao obter os preços da API: ', error);
+            } finally {
+                setLoading(false); // Definindo loading como falso após a obtenção dos dados
             }
-        }, 3000); // Atualizar a cada 3 segundos
+        }, 3000);
     
         return () => clearInterval(interval);
     }, []);
-    
 
-    // Função para simular as operações de compra e venda
+
+    let lastPrice = null;
+
     async function tradingBot(bid: number, ask: number) {
         try {
-            // Verificar se o preço de compra é maior que o preço de venda
-            if (bid > 1.02 * ask) {
-                setBalance(balance + 0.1); // Simular venda de 0.1 BTC
+            if (lastPrice !== null && bid === lastPrice.bid && ask === lastPrice.ask) {
+                return 'manter'; // Se os preços não mudaram, não há necessidade de solicitar entrada novamente
+            }
+            
+            lastPrice = { bid, ask };
+    
+            if (bid >ask  ) { // isto vai ser sempre 
+                const amountToSell = btcBalance;
+                const x = 1000*amountToSell/bid;
+                setBtcBalance(x); // Vender todo o BTC
+                setBalance(balance + amountToSell * bid); // Adicionar o saldo correspondente à venda
                 return 'vender';
-            }
-            // Verificar se o preço de compra é menor que o preço de venda
-            else if (bid < 0.98 * ask) {
-                setBalance(balance - 0.1 * btcPrice); // Simular compra de 0.1 BTC
+            } else if (bid <  ask) {
+                const amountToBuy = balance / ask;
+                setBtcBalance(btcBalance + amountToBuy); // Comprar o máximo possível de BTC
+                setBalance(0); // Definir saldo como zero após comprar BTC
                 return 'comprar';
-            }
-            else {
+            } else {
                 return 'manter';
             }
         } catch (error) {
@@ -48,16 +62,34 @@ function TradingBotVisualizer() {
             return 'manter';
         }
     }
+    
+    
 
     return (
-        <main>
-        <div>
-            <h1>Trading Bot Visualizer</h1>
-            <p>Saldo: ${balance.toFixed(2)}</p>
-            <p>Preço atual do BTC/USD: ${btcPrice.toFixed(2)}</p>
-            <p>Ação sugerida: {action}</p>
+        <div className='pt-50 w-80 justify-center mx-auto'>
+            <h1 className=' text-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-2 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>Trading Bot Visualizer</h1>
+            <img className="w-full py-10" src="bug.png" alt="Bug Image" />
+            <p className=' text-center font-mono text-sm'>Saldo: ${balance.toFixed(2)}</p>
+            {loading ? ( 
+                <div className="flex justify-center items-center">
+                    <TailSpin
+                        height={80}
+                        width={80}
+                        color="#FFFFFF"
+                        ariaLabel="tail-spin-loading"
+                        radius={1}
+                        wrapperStyle={{}}
+                        wrapperClass=""
+                        visible={true}
+                    />
+                </div>
+            ) : (
+                <>
+                    <p className='text-center font-mono text-sm'>Preço atual do BTC/USD: ${btcPrice.toFixed(2)}</p>
+                    <p className='text-center font-mono text-sm'>Ação sugerida: {action}</p>
+                </>
+            )}
         </div>
-        </main>
     );
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,57 @@
+"use client"
+
+import { motion } from "framer-motion";
 import Image from "next/image";
 import TradingBotVisualizer from "./bot";
+import { TailSpin } from 'react-loader-spinner';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Hackathon
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-          >
-            By Hackaloiros_3.0 
-          </a>
-        </div>
-      </div>
-      <TradingBotVisualizer/>
+    <main className="custom-gradient flex flex-col items-center justify-between">
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        className="w-full flex flex-col align-center mt-4"
+      >
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.2, duration: 1 }}
+          className="z-10 w-full items-center justify-between font-mono text-sm lg:flex"
+        >
+        <style>
+        {`
+          .custom-gradient {
+            background: linear-gradient(45deg, #337d27, #265d1d, #1a3e13, #0d1f0a, #000000);
+            width: 100vw;
+            height: 100vh;
+          }
+        `}
+      </style>
+          <p className="top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-2 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            <motion.span>Hackathon</motion.span>
+          </p>
+          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+            <motion.a
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 0.5, duration: 1 }}
+              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            >
+              By <span>Hackaloiros_3.0</span>
+            </motion.a>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.8, duration: 1 }}
+          className="w-full"
+        >
+          <TradingBotVisualizer />
+        </motion.div>
+      </motion.div>
     </main>
   );
 }
